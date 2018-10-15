50c0777d61e5ab804e5fd328616f3a3f76a732ad
abejide001 <abejidefemi1@gmail.com>
2018-10-15 14:03:47 +0100

    Write the implemetation for GET /product/id

--- server/routes/product-routes.js
+++ server/routes/product-routes.js
@@ -3,5 +3,6 @@ import Product from '../src/controller/Product';
 
 const router = express.Router();
 router.get('/', Product.getAll);
+router.get('/:id', Product.getOne);
 
 export default router;
--- server/src/controller/Product.js
+++ server/src/controller/Product.js
@@ -5,5 +5,13 @@ const Product = {
     const products = ProductModel.findAll();
     return res.send(products);
   },
+  getOne(req, res) {
+    const { id } = req.params;
+    const product = ProductModel.findOne(id);
+    if (!product) {
+      return res.status(404).send({ message: 'product not found' });
+    }
+    return res.send(product);
+  },
 };
 export default Product;
