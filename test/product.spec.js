5655d784c6f304500501c2c4074ca97bbde2e1ad
abejide001 <abejidefemi1@gmail.com>
2018-10-15 14:01:24 +0100

    Write test for GET /product/id endpoint

--- test/product.spec.js
+++ test/product.spec.js
@@ -5,7 +5,7 @@ import server from '../index';
 
 chai.use(chaiHttp);
 const should = chai.should();
-
+const { expect } = chai;
 describe('GET /products', () => {
   it('should return 200', (done) => {
     chai.request(server)
@@ -13,6 +13,24 @@ describe('GET /products', () => {
       .end((err, res) => {
         res.should.have.status(200);
         done();
+      });
+  });
+});
+describe('GET /products/:id', () => {
+  it('should return 404 if an invalid id is passed', (done) => {
+    chai.request(server)
+      .get(`/api/v1/products/${9}`)
+      .end((err, res) => {
+        res.should.have.status(404);
+        done(err);
+      });
+  });
+  it('should be an object', (done) => {
+    chai.request(server)
+      .get(`/api/v1/products/${1}`)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        done(err);
       });
   });
 });
