bf5bcf7ed7b8487c9c5a4f7cb6869ce2c8302ff9
abejide001 <abejidefemi1@gmail.com>
2018-10-21 00:31:00 +0100

    Add more test for the sale model

--- test/saleModel.spec.js
+++ test/saleModel.spec.js
@@ -10,4 +10,49 @@ describe('Sale Model', () => {
     expect(SaleModel.findAll().length).to.equal(count + 1);
     done();
   });
+  it('should not return a sale for a sale id that does not exist', (done) => {
+    const count = SaleModel.findAll().length;
+    expect(SaleModel.findOne(count + 1)).to.not.exist;
+    done();
+  });
+  it('should not return errors for a valid sale', (done) => {
+    const sale = SaleModel.create({
+      productName: 'nike jordan',
+      quantitySold: 2,
+    });
+    expect(sale.error).to.not.exist;
+    done();
+  });
+  it('should return errors for an invalid sale name', (done) => {
+    const sale = SaleModel.validate({
+      productName: 'naa',
+      quantitySold: 2,
+    });
+    expect(sale.error).to.exist;
+    expect(sale.error.details.length).to.equal(1);
+    expect(sale.error.details[0].message).to.equal('"productName" length must be at least 5 characters long');
+    done();
+  });
+  it('should return errors for an sale with invalid field "id"', (done) => {
+    const sale = SaleModel.validate({
+      id: 7,
+      productName: 'nike air',
+      quantitySold: 2,
+    });
+    expect(sale.error).to.exist;
+    expect(sale.error.details.length).to.equal(1);
+    expect(sale.error.details[0].message).to.equal('"id" is not allowed');
+    done();
+  });
+  it('should return errors for an sale with invalid field "randomField"', (done) => {
+    const sale = SaleModel.validate({
+      productName: 'air max',
+      quantitySold: 2,
+      randomField: 'random',
+    });
+    expect(sale.error).to.exist;
+    expect(sale.error.details.length).to.equal(1);
+    expect(sale.error.details[0].message).to.equal('"randomField" is not allowed');
+    done();
+  });
 });
