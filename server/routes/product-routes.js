9818945b991ce3036586974225080ad0080c12ed
abejide001 <abejidefemi1@gmail.com>
2018-10-23 18:09:28 +0100

    [chore #161397756] Remove unncessary folders

--- server/routes/product-routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import Product from '../src/controller/Product';
-
-const router = express.Router();
-router.get('/', Product.getAll);
-router.get('/:id', Product.getOne);
-router.post('/', Product.create);
-
-export default router;
