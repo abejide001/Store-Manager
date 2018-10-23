4f94f2c39840b4d1c5ec8a456614684b83294d93
abejide001 <abejidefemi1@gmail.com>
2018-10-23 15:17:38 +0100

    [chore #161397756] Use bodyparser to pass in data into the response body

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import product from './server/routes/product-routes';
-import sale from './server/routes/sale-routes';
+import bodyParser from 'body-parser';
+import product from './server/src/routes/product-routes';
+import sale from './server/src/routes/sale-routes';
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 app.use('/api/v1/products', product);
 app.use('/api/v1/sales', sale);
 
@@ -12,6 +13,7 @@ app.get('/api/v1', (req, res) => {
 });
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
+  /* eslint-disable no-console */
   console.log(`App listening on port ${port}!`);
 });
 export default server;
