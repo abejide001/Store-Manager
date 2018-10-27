232e13ca93a2b32a048a492a944e35b0c6f4ddfa
abejide001 <abejidefemi1@gmail.com>
2018-10-27 17:47:21 +0100

    [chore #161526230] Create product model method

--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -1,41 +1,35 @@
+import pool from '../../db-config/database_connection';
 import Validation from '../helper/product-helper';
 
-class Product {
-  constructor() {
-    this.products = [{
-      id: 1,
-      name: 'air max',
-      price: 1000,
-      quantityInInventory: 2,
-    },
-    {
-      id: 2,
-      name: 'nike air',
-      price: 1000,
-      quantityInInventory: 3,
-    }];
-  }
-
+const Product = {
   findAll() {
     return this.products;
-  }
+  },
 
   findOne(id) {
     return this.products.find(product => product.id === Number(id));
-  }
+  },
 
-  create(data) {
+  async create(data) {
     const validated = Validation.validate(data);
     if (validated.errors.length !== 0) {
       return validated;
     }
-    const { name, price, quantityInInventory } = validated.value;
-    const product = {
-      id: this.products.length + 1, name, price, quantityInInventory,
-    };
-    this.products.push(product);
-    validated.value = product;
-    return validated;
-  }
-}
+    const { name, price, quantity_in_inventory } = validated.value;
+    try {
+      const result = await pool.query(
+        'INSERT INTO products(name, price, quantity_in_inventory) VALUES($1, $2, $3) RETURNING id',
+        [name, price, quantity_in_inventory],
+      );
+      const product = {
+        id: result.rows[0].id, name, price, quantity_in_inventory,
+      };
+      validated.value = product;
+      return validated;
+    } catch (err) {
+      validated.errors.push(err.message);
+      return validated;
+    }
+  },
+};
 export default new Product();
