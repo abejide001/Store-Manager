32f53a3c59c7ef306c3a70ba8bb0b841d5d409bd
abejide001 <abejidefemi1@gmail.com>
2018-10-27 16:07:03 +0100

    [chore #161521900] Change fields naming convention

--- server/src/schema/migration.sql
+++ server/src/schema/migration.sql
@@ -2,11 +2,11 @@ CREATE TABLE products(
     id SERIAL PRIMARY KEY,
     name VARCHAR(40) NOT NULL,
     price INTEGER NOT NULL,
-    quantityininventory INTEGER NOT NULL
+    quantity_in_inventory INTEGER NOT NULL
 );
 
 CREATE TABLE sales(
     id SERIAL PRIMARY KEY,
-    productname VARCHAR(40) NOT NULL,
-    quantitysold INTEGER NOT NULL
+    product_name VARCHAR(40) NOT NULL,
+    quantity_sold INTEGER NOT NULL
 );
