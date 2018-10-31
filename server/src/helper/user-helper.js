939d6a34dceae235948119adecd7fd243f2a8a98
abejide001 <abejidefemi1@gmail.com>
2018-10-31 09:22:24 +0100

    [chore #161532537] Add email validation

--- server/src/helper/user-helper.js
+++ server/src/helper/user-helper.js
@@ -1,52 +1,6 @@
-const Roles = [
-  'admin',
-  'agent',
-];
-
 const Validation = {
-  validateRole(validateUser) {
-    if (!validateUser.value.role) {
-      validateUser.errors.push('"role" is required');
-      return validateUser;
-    }
-    if (!Roles.includes(validateUser.value.role)) {
-      validateUser.errors.push(`"role" should be one of: '${Roles.join(',')}`);
-    }
-    return validateUser;
-  },
-
-  validateFullName(validateUser) {
-    if (!validateUser.value.fullname) {
-      validateUser.errors.push('"fullname" is required');
-      return validateUser;
-    }
-    if (validateUser.value.fullname.length < 5) {
-      validateUser.errors.push('"fullname" length must be at least 5 characters long');
-    }
-    if (validateUser.value.fullname.length > 15) {
-      validateUser.errors.push('"fullname" length must be at most 15 characters long');
-    }
-    return validateUser;
-  },
-  validateEmail(validateUser) {
-    if (!validateUser.value.email) {
-      validateUser.errors.push('"email" is required');
-      return validateUser;
-    }
-    if (validateUser.value.email.length < 10) {
-      validateUser.errors.push('"email" must be greater than 10');
-    }
-    return validateUser;
-  },
-  validate(user) {
-    const validateUser = {};
-    validateUser.errors = [];
-    validateUser.value = user;
-    this.validateRole(validateUser);
-    this.validateFullName(validateUser);
-    this.validateEmail(validateUser);
-    return validateUser;
+  isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
   },
 };
-
 export default Validation;
