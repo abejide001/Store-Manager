d9a4cf3eb97a431519efc8d1d88a3915d3134d97
abejide001 <abejidefemi1@gmail.com>
2018-10-31 15:31:59 +0100

    [chore #161616963] Add admin  middleware to routes

--- server/src/routes/user-routes.js
+++ server/src/routes/user-routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import User from '../controller/User';
+import Auth from '../middleware/Auth';
 
 const router = express.Router();
 
-router.post('/signup', User.register);
+router.post('/signup', Auth.verifyToken, Auth.verifyAdmin, User.register);
 router.post('/signin', User.login);
 
 export default router;
