bfeaa6ec3cbacaebe817d580529c622aff4e396f
abejide001 <abejidefemi1@gmail.com>
2018-10-31 13:04:25 +0100

    [chore #161617390] Add middleware to products and sales routes

--- server/src/routes/sale-routes.js
+++ server/src/routes/sale-routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import Sale from '../controller/Sale';
+import Auth from '../../db-config/database_connection';
 
 const router = express.Router();
 
-router.get('/', Sale.getAllSales);
-router.get('/:id', Sale.getOneSale);
-router.post('/', Sale.createSale);
+router.get('/', Auth.verfifyToken, Sale.getAllSales);
+router.get('/:id', Auth.verfifyToken, Sale.getOneSale);
+router.post('/', Auth.verfifyToken, Sale.createSale);
 export default router;
