3c129ec7beadf6ed6abc48d8f2feee85857c1ca7
abejide001 <abejidefemi1@gmail.com>
2018-11-01 11:39:04 +0100

    Add middleware to route

--- server/src/routes/user-routes.js
+++ server/src/routes/user-routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import User from '../controller/User';
 import Auth from '../middleware/Auth';
+import Validate from '../middleware/user';
 
 const router = express.Router();
 
-router.post('/signup', Auth.verifyToken, Auth.verifyAdmin, User.register);
-router.post('/signin', User.login);
+router.post('/signup', Auth.verifyToken, Auth.verifyAdmin, Validate.validateSignUp, User.register);
+router.post('/signin', Validate.validateSignin, User.login);
 
 export default router;
