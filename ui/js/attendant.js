48d14e0b8f99d5dfcd11744fe7a8e1442c483514
abejide001 <abejidefemi1@gmail.com>
2018-11-15 20:44:01 +0100

    [chore] Fetch product for attendant page and fix eslint errors

--- ui/js/attendant.js
+++ ui/js/attendant.js
@@ -1,22 +1,23 @@
 // Assign selectors
-const removeButton = document.querySelectorAll(".btn-danger"),
-  addToCart = document.querySelectorAll(".product-button"),
-  quantityInput = document.querySelectorAll(".cart-quantity-input");
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", ready);
+const removeButton = document.querySelectorAll('.btn-danger'),
+  addToCart = document.querySelectorAll('.product-button'),
+  quantityInput = document.querySelectorAll('.cart-quantity-input'),
+  token = localStorage.getItem('authToken');
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', ready);
 } else {
   ready();
 }
 
 function ready() {
-  removeButton.forEach(button => button.addEventListener("click", removeProduct));
-  addToCart.forEach(item => item.addEventListener("click", addToCartButton));
-  quantityInput.forEach(input => input.addEventListener("change", quantityChanged));
+  removeButton.forEach(button => button.addEventListener('click', removeProduct));
+  addToCart.forEach(item => item.addEventListener('click', addToCartButton));
+  quantityInput.forEach(input => input.addEventListener('change', quantityChanged));
 }
 
 function removeProduct(event) {
-  event.target.parentElement.parentElement.remove()
-  updateCartTotal()
+  event.target.parentElement.parentElement.remove();
+  updateCartTotal();
 }
 
 function quantityChanged(event) {
@@ -28,40 +29,39 @@ function quantityChanged(event) {
 
 function updateCartTotal() {
   let total = 0;
-  const cartItemContainer = document.querySelectorAll(".cart-items")[0],
-  cartRows = cartItemContainer.querySelectorAll(".cart-row");
-  cartRows.forEach(cart => {
-    const priceElement = cart.querySelectorAll(".cart-price")[0];
+  const cartItemContainer = document.querySelectorAll('.cart-items')[0],
+    cartRows = cartItemContainer.querySelectorAll('.cart-row');
+  cartRows.forEach((cart) => {
+    const priceElement = cart.querySelectorAll('.cart-price')[0];
     const quantityElement = cart.querySelectorAll(
-      ".cart-quantity-input"
+      '.cart-quantity-input',
     )[0];
     const price = parseInt(priceElement.innerText.replace(/\W/gi, ''));
     const quantity = Number(quantityElement.value);
     total += (price * quantity);
   });
-  document.querySelectorAll(".cart-total-price")[0].innerText =
-    "#" + total;
+  document.querySelectorAll('.cart-total-price')[0].innerText = `#${total}`;
 }
 
 function addToCartButton(event) {
   const product = event.target.parentElement.parentElement,
-    title = product.querySelectorAll(".product-name")[0].innerText,
-    price = product.querySelectorAll(".product-price")[0].innerText,
-    image = product.querySelectorAll(".product-image")[0].src;
+    title = product.querySelectorAll('.product-name')[0].innerText,
+    price = product.querySelectorAll('.product-price')[0].innerText,
+    image = product.querySelectorAll('.product-image')[0].src;
   addItemToCart(title, price, image);
-  updateCartTotal()
+  updateCartTotal();
 }
 
 function addItemToCart(title, price, image) {
-  const cartRow = document.createElement("div");
-  cartRow.classList.add("cart-row");
-  const cartItems = document.querySelectorAll(".cart-items")[0],
-      cartItemNames = document.querySelectorAll('.cart-item-title');
-      cartItemNames.forEach(cartItemName => {
-        if (cartItemName.innerText === title) {
-          throw new Error('item already added')
-        }
-      })
+  const cartRow = document.createElement('div');
+  cartRow.classList.add('cart-row');
+  const cartItems = document.querySelectorAll('.cart-items')[0],
+    cartItemNames = document.querySelectorAll('.cart-item-title');
+  cartItemNames.forEach((cartItemName) => {
+    if (cartItemName.innerText === title) {
+      throw new Error('item already added');
+    }
+  });
   const cartRowContent = `
   <div class="cart-item cart-column">
       <img class="cart-item-image" src="${image}" width="100" height="100">
@@ -74,6 +74,29 @@ function addItemToCart(title, price, image) {
   </div>`;
   cartRow.innerHTML = cartRowContent;
   cartItems.append(cartRow);
-  cartRow.querySelectorAll('.btn-danger')[0].addEventListener('click', removeProduct)
-  cartRow.querySelectorAll('.cart-quantity-input')[0].addEventListener('change', quantityChanged)
+  cartRow.querySelectorAll('.btn-danger')[0].addEventListener('click', removeProduct);
+  cartRow.querySelectorAll('.cart-quantity-input')[0].addEventListener('change', quantityChanged);
 }
+fetch('http://store-manager-store.herokuapp.com/api/v1/products', {
+  headers: {
+    'x-access-token': token,
+  },
+})
+  .then(res => res.json())
+  .then((data) => {
+    let output = '';
+    const items = data.products.value;
+    items.forEach((item) => {
+      output += `
+     <div class="product">
+      <img src=${item.product_image} class="product-image">   
+      <p class="product-name">${item.name}</p>
+      <p class="product-price">#${item.price.toLocaleString()}</p>
+      <p>
+        <button class="product-button" type="button">ADD TO CART</button>
+      </p>
+      </div>
+    `;
+    });
+    document.querySelector('.products').innerHTML = output;
+  });
